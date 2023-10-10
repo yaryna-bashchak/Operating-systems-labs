216a474e2fba3b4b7f2b20a96e2e7a357e2e93f9
yaryna-bashchak <yaryna.bashchak@gmail.com>
2023-10-10 21:54:06 +0300

    create simple program

---
+++ lab1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace lab1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numberOfPhysicalPages = 15;
+        int maxProcessCount = 5;
+        uint startPageNumber = 0x00010000;
+
+        var kernel = new Kernel(maxProcessCount, numberOfPhysicalPages, startPageNumber);
+
+        var p1 = new Process(1, 20, 100);
+        var p2 = new Process(2, 25, 100);
+
+        kernel.AddProcess(p1);
+        kernel.AddProcess(p2);
+
+        kernel.Run();
+        kernel.Run();
+    }
+}
