a1b972fba0a43ae3e594e27c9b08d2d33fc6d11c
yaryna-bashchak <yaryna.bashchak@gmail.com>
2023-12-22 00:56:09 +0200

    add examples of using of new functionality to Main

--- lab4-5/Program.cs
+++ lab4-5/Program.cs
@@ -18,19 +18,67 @@ class Program
         fileSystem.Stat("/");
         fileSystem.Stat("dir1/");
         fileSystem.Stat("dir1");
-        fileSystem.ChangeDirectory("dir1");
-        fileSystem.Create("file3");
+        // fileSystem.ChangeDirectory("dir1");
+        // fileSystem.Create("file3");
+
+        // symlink with regular file
         fileSystem.Ls();
-        fileSystem.Ls("dir3/");
-        fileSystem.Stat();
-        fileSystem.Stat("/");
-        fileSystem.Stat("/dir1");
-        fileSystem.Stat("/dir1/");
-        fileSystem.Stat("dir3");
-        fileSystem.Stat("file3");
-        fileSystem.Stat("./file3");
-        fileSystem.Stat("../file1");
-        fileSystem.Stat("/dir1/file3");
+        fileSystem.CreateSymlink("dir1/file1", "sym_file1");
+        fileSystem.Ls();
+        fileSystem.Stat("sym_file1");
+        fileSystem.Stat("dir1/file1");
+
+        var fd1 = fileSystem.Open("sym_file1");
+        fileSystem.Truncate("dir1/file1", 128);
+        byte[] byteArray = new byte[20];
+
+        for (int i = 0; i < byteArray.Length; i++)
+        {
+            byteArray[i] = (byte)i;
+        }
+
+        fileSystem.Write(fd1, byteArray);
+        fileSystem.Stat("sym_file1");
+        fileSystem.Stat("dir1/file1");
+
+        // fileSystem.Close(fd1);
+
+        // symlink with directories
+        fileSystem.Ls();
+        fileSystem.CreateSymlink("dir1/dir3", "dir2/sym_dir3");
+        fileSystem.Ls("dir2");
+
+        var fd2 = fileSystem.Open("dir2/sym_dir3/./file1"); // doesn't exist
+        var fd3 = fileSystem.Open("dir2/sym_dir3/../file1"); // exists
+
+        // more complex example
+        fileSystem.CreateSymlink("/dir2", "dir1/sym_dir2");
+        fileSystem.Stat("dir2/sym_dir3/../sym_dir2");
+        fileSystem.Stat("dir2");
+
+        // fileSystem.Ls("dir3/");
+        // fileSystem.Stat();
+        // fileSystem.Stat("/");
+        // fileSystem.Stat("/dir1");
+        // fileSystem.Stat("/dir1/");
+        // fileSystem.Stat("dir3");
+        // fileSystem.Stat("file3");
+        // fileSystem.Stat("./file3");
+        // fileSystem.Stat("../dir1");
+        // fileSystem.Stat("../file1");
+        // fileSystem.Stat("/dir1/file3");
+
+
+        // fileSystem.ChangeDirectory("/");
+        // fileSystem.Ls("dir1");
+        // fileSystem.RemoveDirectory("dir1");
+        // fileSystem.Unlink("dir1/file1");
+        // fileSystem.Unlink("dir1/file2");
+        // fileSystem.Unlink("dir1/file3");
+        // fileSystem.RemoveDirectory("dir1/dir3");
+        // fileSystem.Ls("dir1");
+        // fileSystem.RemoveDirectory("dir1");
+
         // fileSystem.Create("file1.txt");
         // fileSystem.Create("file2.txt");
         // fileSystem.Truncate("file2.txt", 128);
